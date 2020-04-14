9469ab67604644964d5e084ed5574b7d885545b3
suchimauz <alexey.fedulov98@gmail.com>
2020-04-14 21:22:46 +0300

    Fix footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,13 +149,10 @@
                 <footer class="footer">
                     <div class="row">
                         <div class="col-md-6 col-sm-12">
-                            <p>Pampered by<a href="#" class="text-dark" >Hencework</a> © 2019</p>
+                            <p>Сбербанк © 2020</p>
                         </div>
                         <div class="col-md-6 col-sm-12">
-                            <p class="d-inline-block">Follow us</p>
-                            <a href="#" class="d-inline-block btn btn-icon btn-icon-only btn-indigo btn-icon-style-4"><span class="btn-icon-wrap"><i class="fa fa-facebook"></i></span></a>
-                            <a href="#" class="d-inline-block btn btn-icon btn-icon-only btn-indigo btn-icon-style-4"><span class="btn-icon-wrap"><i class="fa fa-twitter"></i></span></a>
-                            <a href="#" class="d-inline-block btn btn-icon btn-icon-only btn-indigo btn-icon-style-4"><span class="btn-icon-wrap"><i class="fa fa-google-plus"></i></span></a>
+                            <p class="d-inline-block">Марков Александр. КТ-41-16</p>
                         </div>
                     </div>
                 </footer>
